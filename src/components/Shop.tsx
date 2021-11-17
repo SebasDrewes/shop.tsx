ec9ddffe007826b60ac0b13f62ca964197a77ed7
Sebastian Drewes <sebas.drewes@gmail.com>
2021-11-17 20:28:58 -0300

    Added map and types

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,6 +1,25 @@
 import useProducts from "./useProducts";
-
+type ProductsTypes = {
+  id: number;
+  title: string;
+  image: string;
+  description: string;
+};
 export default function Shop() {
-  useProducts();
-  return <h1>Welcome to Shop!</h1>;
+  const products = useProducts();
+  return (
+    <>
+      <h1>Welcome to Shop!</h1>
+      {products.map((product: ProductsTypes) => {
+        return (
+          <div key={product.id}>
+            <h3>{product.title}</h3>
+            <img src={product.image} alt={product.title} />
+            <p>{product.description}</p>
+            <p>{product.price}</p>
+          </div>
+        );
+      })}
+    </>
+  );
 }
