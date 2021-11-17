4b5673fd98ec6df0ac9bb196d43d9e4177831d9c
Sebastian Drewes <sebas.drewes@gmail.com>
2021-11-17 13:01:29 -0300

    Implemented right drawer cart

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import "./Navbar.scss";
+import Link from "@mui/material/Link";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import SearchIcon from "@mui/icons-material/Search";
+import Cart from "./Cart";
+
+export default function Navbar() {
+  const [state, setState] = useState(false);
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setState(open);
+    };
+
+  return (
+    <nav className="navbar">
+      <Link href="/" underline="none" color="white">
+        Inicio
+      </Link>
+      <Link href="/shop" underline="none" color="white">
+        Shop
+      </Link>
+      <Link href="/search">
+        <SearchIcon sx={{ fontSize: 90, color: "white" }} />
+      </Link>
+
+      <Button onClick={toggleDrawer(true)}>
+        <ShoppingCartIcon sx={{ fontSize: 90, color: "white" }} />
+      </Button>
+      <Drawer anchor={"right"} open={state} onClose={toggleDrawer(false)}>
+        <Cart />
+      </Drawer>
+    </nav>
+  );
+}
