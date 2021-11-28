87a05aa4ed8bbc00f15ef66d8f5a944abdbfe88e
Sebastian Drewes <sebas.drewes@gmail.com>
2021-11-28 11:55:10 -0300

    Improved product style

--- src/components/Shop/ShopItem/ShopItem.tsx
+++ src/components/Shop/ShopItem/ShopItem.tsx
@@ -19,23 +19,16 @@ export default function ShopItem({ product }: ShopItemsPropTypes) {
   console.log(product);
   return (
     <div key={product.id} className="shopProduct">
-      <div className="shopProductImageContainer">
-        <img
-          src={product.image}
-          className="shopProductImage"
-          alt={product.title}
-        />
-      </div>
-      <div className="productInfo">
-        <h3 className="productTitle">{product.title}</h3>
-        <p>${product.price}</p>
-        <button
-          className="shopProductButton"
-          onClick={() => addToCart(product)}
-        >
-          Add to cart
-        </button>
-      </div>
+      <img
+        src={product.image}
+        className="shopProductImage"
+        alt={product.title}
+      />
+      <h3 className="productTitle">{product.title}</h3>
+      <p>${product.price}</p>
+      <button className="shopProductButton" onClick={() => addToCart(product)}>
+        Add to cart
+      </button>
     </div>
   );
 }
