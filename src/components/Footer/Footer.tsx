18250777c0f5b754fd7f4f16e866cbcfbfeabdaf
Sebastian Drewes <sebas.drewes@gmail.com>
2021-11-28 23:48:18 -0300

    finished footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,27 @@
 import "./Footer.scss";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 export default function Footer() {
-  return <footer className="footer">Hello World</footer>;
+  return (
+    <footer className="footer">
+      Made by SebasDrewes
+      <a
+        href="https://linkedin.com/in/sebasdrewes"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="Github"
+      >
+        <LinkedInIcon className="LinkedInIcon" />
+      </a>
+      <a
+        href="https://github.com/sebasdrewes"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="Github"
+      >
+        <GitHubIcon className="GitHubIcon" />
+      </a>
+    </footer>
+  );
 }
