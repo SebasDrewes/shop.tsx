1d5d123b99ed476b742e74fd98b4d9b503d9f996
Sebastian Drewes <sebas.drewes@gmail.com>
2021-11-30 12:10:16 -0300

    Added cart is empty

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,10 +1,16 @@
 import { useContext } from "react";
 import { ProductsContext } from "../../context/ProductsContext";
+import { useNavigate } from "react-router-dom";
 import CartItem from "./CartItem/CartItem";
 import CloseIcon from "@mui/icons-material/Close";
 import "./Cart.scss";
 export default function Cart({ close }: any) {
+  const navigate = useNavigate();
   const products = useContext(ProductsContext);
+  function handleBrowseClick() {
+    navigate("/shop");
+    close(false);
+  }
   console.log(products);
   return (
     <div className="shoppingCart">
@@ -16,11 +22,21 @@ export default function Cart({ close }: any) {
         />
       </div>
       <div className="cartProductsContainer">
-        {products && products.length > 0
-          ? products.map((product) => {
-              return <CartItem product={product} key={product.id} />;
-            })
-          : "Your cart is empty"}
+        {products && products.length > 0 ? (
+          products.map((product) => {
+            return <CartItem product={product} key={product.id} />;
+          })
+        ) : (
+          <>
+            <p className="emptyCart">Your cart is empty</p>
+            <button
+              className="checkoutButton"
+              onClick={() => handleBrowseClick()}
+            >
+              Browse Products
+            </button>
+          </>
+        )}
       </div>
       {products && products.length > 0 ? (
         <div className="totalContainer">
