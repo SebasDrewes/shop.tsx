dbbd267c3cbf6da0dcad29f916c585c3a0643b3c
Sebastian Drewes <sebas.drewes@gmail.com>
2021-12-02 22:35:23 -0300

    refactor useProducts

--- src/hooks/useProducts.tsx
+++ src/hooks/useProducts.tsx
@@ -3,16 +3,7 @@ export default function useProducts(id?: string) {
   const [products, setProducts] = useState([]);
   useEffect(() => {
     (async function getData() {
-      if (id) {
-        const data = await fetch(`https://fakestoreapi.com/products/${id}`);
-        const products = await data.json();
-        const clothing = products.filter(
-          (product: { category: string }) =>
-            product.category === `men's clothing` ||
-            product.category === `women's clothing`
-        );
-        setProducts(clothing);
-      } else {
+      try {
         const data = await fetch("https://fakestoreapi.com/products/");
         const products = await data.json();
         const clothing = products.filter(
@@ -21,6 +12,8 @@ export default function useProducts(id?: string) {
             product.category === `women's clothing`
         );
         setProducts(clothing);
+      } catch {
+        throw Error("Unable to fetch data");
       }
     })();
   }, [id]);
