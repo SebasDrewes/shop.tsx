5f91764a905124d9ec1c1bd8e4f5fb83e815055c
Sebastian Drewes <sebas.drewes@gmail.com>
2021-12-05 01:10:23 -0300

    Finished featured

--- src/components/Home/Featured/CustomFeature/CustomFeature.tsx
+++ src/components/Home/Featured/CustomFeature/CustomFeature.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./CustomFeature.scss";
 import { useNavigate } from "react-router-dom";
 
@@ -12,10 +13,34 @@ export default function CustomFeature({
   category,
 }: FeaturedTypes) {
   const navigate = useNavigate();
+  const [position, setPosition] = useState({
+    text: "0%",
+    shopNow: "75%",
+  });
+  const [underline, setUnderline] = useState("none");
+  function handleMouseEnter() {
+    setPosition({ ...position, text: "30%", shopNow: "50%" });
+    setUnderline("underline");
+  }
+  function handleMouseLeave() {
+    setPosition({ ...position, text: "10%", shopNow: "75%" });
+    setUnderline("none");
+  }
   return (
-    <div className="featured">
-      <h1 className="dealText">{text}</h1>
-      <h1 className="shopNowFeature">Shop now</h1>
+    <div
+      className="featured"
+      onMouseEnter={() => handleMouseEnter()}
+      onMouseLeave={() => handleMouseLeave()}
+    >
+      <h1 className="dealText" style={{ top: position.text }}>
+        {text}
+      </h1>
+      <h1
+        className="shopNowFeature"
+        style={{ top: position.shopNow, textDecoration: underline }}
+      >
+        Shop now
+      </h1>
       <img
         src={image}
         alt={text}
